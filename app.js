94996dfe5359b6405ac837e628eff80313d9c5f3
Nidhikhanpara102 <Nidhikhanpara102@gmail.com>
2024-04-09 17:27:15 -0400

    App.js updated. Routes imported to app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
 // Initialize Express app
 const app = express();
 
@@ -15,6 +22,13 @@ mongoose.connect('mongodb+srv://nkhanpara9106:4FZHlWK1rUKcxlXP@webassignment4.x7
 
 // Middleware
 app.use(express.json());
+
+// routes
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/carts', cartRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
